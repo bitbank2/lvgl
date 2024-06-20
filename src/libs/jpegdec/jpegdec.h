d37cf359aa3e8de516af99c9273563199d8d14d5
Laurence Bank <laurencebank@Laurences-Air.home>
2024-06-20 19:45:39 +0100

    removing dependencies to build on UEFI

--- src/libs/jpegdec/jpegdec.h
+++ src/libs/jpegdec/jpegdec.h
@@ -16,9 +16,7 @@
 // fixed to Linux for LVGL
 #define __LINUX__
 #if defined( __MACH__ ) || defined( __LINUX__ ) || defined( __MCUXPRESSO ) || defined( ESP_PLATFORM )
-    #include <stdlib.h>
     #include <string.h>
-    #include <stdint.h>
     #include <stdio.h>
 #else
     #include <Arduino.h>
